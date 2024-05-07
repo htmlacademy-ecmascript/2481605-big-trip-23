5260e08c47cff8b79e8094cede3a06f6abaff421
Sasha <pratov99@gmail.com>
2024-05-07 03:02:15 +0300

    Шаблонизируй это

---
+++ src/mock/destinations.js
@@ -0,0 +1,29 @@
+export const destinations = [{
+  id: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e043',
+  description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+  name: 'Chamonix',
+  pictures: [{
+    src: 'https://loremflickr.com/248/152?random=3124',
+    description: 'Chamonix parliament building'
+  }
+  ]
+},
+{
+  id: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+  description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+  name: 'Moscow',
+  pictures: [{
+    src: 'https://loremflickr.com/248/152?random=4214',
+    description: 'Moscow is a heart of Russia'
+  }]
+},
+{
+  id: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e044',
+  description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+  name: 'London',
+  pictures: [{
+    src: 'https://loremflickr.com/248/152?random=3254',
+    description: 'London is a capital of GB'
+  }]
+}
+];
